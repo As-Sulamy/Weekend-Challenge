4f4619ed7766570b8a07083aa66e340b1ccdd798
As-Sulamy Concepts <sulaimanbello249@gmail.com>
2025-03-17 10:02:57 +0100

    Add files via upload

---
+++ cheaper_currency.py
@@ -0,0 +1,10 @@
+Pesos = int(input("Enter the price in Pesos: "))
+Dollars = int(input("Enter the price in Dollars: "))
+
+pesos_in_dollars = Pesos * 0.02
+
+if pesos_in_dollars < Dollars:
+  print("Poses")
+else: 
+  print("Dollars")
+  
